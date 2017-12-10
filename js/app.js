b5f2f533fa1ecb20c01c06fc27a667cee8e80f55
K Jerry Alleyne <kjalleyne@hotmail.com>
2017-12-10 00:19:43 +0000

    Adding style data as needed in correspondence with working JavaScript functionality. Randomization of array to push to nodes in progress.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 //$(document).ready(() => {
-
 // popup control functions
 let nameInputDiv = $('#name-input');
 let nameinput = $('form > input');
 let submitName = $('#name-input form button');
 let nameFeed = $('.nameFeed');
+let items = $('.item');
+//const icons = ['☺','☺','☆','★','♡','❤'];
+const icons = ['1','2','3','4','5','6'];
+const togameboard = [];
+const playLog = [];
+const logs = console.log;
+
 nameInputDiv.toggleClass('hidden');
 nameInputDiv.toggleClass('popup');
 
@@ -12,22 +18,96 @@ nameInputDiv.toggleClass('popup');
 nameinput.change(() => {
 //    nameInputDiv.toggleClass('hidden');
     nameInputDiv.toggleClass('popup');
-    nameFeed[1].text(nameinput.val());
+    nameFeed[1].html(nameinput.val());
 
 });
+
 submitName.click((event) => {
     nameInputDiv.toggleClass('popup');
     event.preventDefault();
     
 });
 
-//feed input name to display size
+//feed input name to display size as typing
 nameinput.on('keyup', () => {
-    nameFeed.text(nameinput.val());
+    nameFeed.html(nameinput.val());
 
 });
 
+$(document).ready(() => {
+    
+// append each of the given icons to a button on the gameboard twice in random sequence
+// to do this I can push random number from [0 to desired length] to an array and pop any additional copy until it is complete
+// or do this in two halfs
+    let slotsLngth = items.length;
+    let gfxLngth = icons.length;
+    logs(slotsLngth + ':All slots.' + gfxLngth + ':Icons(gfx)count');
 
-let items = $('.items');
+    for (let x = 0; x < slotsLngth; x++) {
+        let z = gfxLngth - 1; //.Math.Random;
+        if (z != 0 && z > slotsLngth) { z = 0}
+        let w = Math.floor((Math.random() * z) + 0);
+        let wo = () => {
+            let y;
+            while (w == 0 || togameboard.forEach(w)) {
+                w = Math.floor((Math.random() * z) + 0);
+                y = w;
+            }
+            return y;
+        };
+        wo;
+        let ww = wo;
+        logs(ww + " return from wo");
+        
+        if (x <= z) { togameboard.push(w);}
+    console.log(togameboard);
+    items[x].append(icons[w] + '☺ temp');
+
+    }
+
+// set a timer after first click which resets to 0 on the second click &&
+// set the action & scoring logic for each two clicks
+    for (let x=0; x < slotsLngth; x++) {
+        let elmnt = items[x];
+        elmnt.addEventListener("click", newPlay);
+        
+    }
+    
+    timer();
+
+}); // end of document.ready
+
+let newPlay = () => {
+    this.addClass("newPlay");
+    logs(this);
+    
+};
+
+const evaluate = (x) => {
+        playLog.push(x);
+        logs(x + "testing");
+//        timer();
+};
+
+const timer = () => {
+    let start = 0;
+    setInterval(moveTimespan, 1000);
+    
+    moveTimespan(start);
+};
+
+const moveTimespan = (x) => {
+    let start = x;
+    x+=1;
+    $('#timer').html(x);
+};
+
+class Docode {
+    constructor(timer, scoreCount) {
+        this.scoreCount = () => {};
+        this.stopListener = () => {};
+        
+        }
+}
 
 //});
